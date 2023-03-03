00f4a7f60ddea3ef483969ee96ec8a35fbc19a0c
TheBarnHome <126877271+TheBarnHome@users.noreply.github.com>
2023-03-03 16:45:17 +0100

    corrections terraform

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,12 +3,18 @@
 sudo apt update
 sudo apt upgrade
 
+# Prerequeries
+sudo apt -y install software-properties-common gnupg2 curl
+
 # Install Ansible
-sudo apt -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt install -y ansible
 
 # Install Terraform
+curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
+sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
 sudo apt install -y terraform
 
 # End
