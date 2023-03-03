b20b602c9af253eb11ae7434d136030e9597b826
TheBarnHome <126877271+TheBarnHome@users.noreply.github.com>
2023-03-03 16:16:21 +0100

    Ajout de l'install terraform

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,9 +3,12 @@
 sudo apt update
 sudo apt upgrade
 
+# Install Ansible
 sudo apt -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
+sudo apt install -y ansible
 
-sudo apt install ansible
+# Install Terraform
+sudo apt install -y terraform
 
 # End
