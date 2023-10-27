75cf497192d538942d0cace45cc17d86d7a9c7ae
TheBarnHome <126877271+TheBarnHome@users.noreply.github.com>
2023-10-28 00:24:18 +0200

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -10,12 +10,8 @@ sudo apt -y install software-properties-common gnupg2 curl
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt install -y ansible
 
-# Install Terraform
-# curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
-# sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
-# sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-# sudo apt install -y terraform
+ansible-galaxy role install geerlingguy.pip
+ansible-galaxy role install geerlingguy.docker
 
 # Install Docker and Code-Server
 
